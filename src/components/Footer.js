49ca0e506bc325fa5677d5ecf9b6c6d7e9351c75
KyleAktr <aktr.contact@gmail.com>
2023-07-20 15:02:44 +0200

    fix responsive bug

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,12 @@
 import React from "react";
+import LogoFooter from "../assets/img/logo_logeuse.png";
 
 const Footer = () => {
   return (
     <div className="footer">
+      <div className="footer-logo">
+        <img src={LogoFooter} alt="logo xenia x logeuse" />
+      </div>
       <div className="footer-content">
         <p>2023© Xenia - Logeuse.com</p>
       </div>
